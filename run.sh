3abe18107557f13fb6d61900b0cc989ecd16420d
ASCIIJK <150874756+ASCIIJK@users.noreply.github.com>
2025-05-13 11:20:45 +0800

    First upload for MiN

---
+++ run.sh
@@ -0,0 +1,6 @@
+python main.py --base_configs configs/base_configs/cifar224.json --model_configs configs/model_configs/MiN.json
+python main.py --base_configs configs/base_configs/cub.json --model_configs configs/model_configs/MiN.json
+python main.py --base_configs configs/base_configs/cifar224.json --model_configs configs/model_configs/MiN.json
+python main.py --base_configs configs/base_configs/cifar224.json --model_configs configs/model_configs/MiN.json
+python main.py --base_configs configs/base_configs/cifar224.json --model_configs configs/model_configs/MiN.json
+python main.py --base_configs configs/base_configs/cifar224.json --model_configs configs/model_configs/MiN.json
